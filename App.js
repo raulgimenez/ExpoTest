3e8ab221f169b0bb969a8806aca0c19eaf8dd44b
raul gimenez valladares <raul.sbd@gmail.com>
2018-01-26 17:18:31 +0100

    Logos
    cositas

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ export default class App extends React.Component {
   };
 
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    if (      !this.state.isLoadingComplete 
+          &&  !this.props.skipLoadingScreen ) {
       return (
         <AppLoading
           startAsync={this._loadResourcesAsync}
